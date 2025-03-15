3055e1ca6d97e9c15259f6495838853c8305ecc7
Lim Ting Zhi <limtingzhi@gmail.com>
2025-03-16 01:47:14 +0800

    feat: setup Material UI

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
+import { CssBaseline } from '@mui/material';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router';
 import App from './pages/App';
 import PackageDetails from './pages/PackageDetails';
 import PageNotFound from './pages/PageNotFound';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
+    <CssBaseline />
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/package/:name" element={<PackageDetails />} />
