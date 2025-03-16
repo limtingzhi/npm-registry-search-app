3f9917f9f6f2324ae588eec3c4526f15a409c572
Lim Ting Zhi <limtingzhi@gmail.com>
2025-03-16 18:49:34 +0800

    feat: style PageNotFound page

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,12 +1,21 @@
-import { NavLink } from 'react-router';
+import { Button, Container, Stack, Typography } from '@mui/material';
+import { Link } from 'react-router';
 
 function PageNotFound() {
   return (
-    <div>
-      <h1>404</h1>
-      <p>We can't seem to find the page you're looking for.</p>
-      <NavLink to="/">Go Home</NavLink>
-    </div>
+    <Container sx={{ paddingY: 3 }}>
+      <Stack spacing={3} sx={{ alignItems: 'center', textAlign: 'center' }}>
+        <Typography gutterBottom variant="h1">
+          404
+        </Typography>
+        <Typography gutterBottom variant="body1">
+          We can't seem to find the page you're looking for.
+        </Typography>
+        <Button component={Link} disableElevation to="/" variant="contained">
+          Go Home
+        </Button>
+      </Stack>
+    </Container>
   );
 }
 
